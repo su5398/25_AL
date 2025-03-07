7721f0566ab9800278e4229ad81fa0e2ad4ab5f1
Kweon <su539@naver.com>
2025-03-07 11:45:04 +0900

    N1954

---
+++ L1/src/N1954.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class N1954 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		
+		for(int i=1; i<T+1; i++) {
+			int N = sc.nextInt();
+			int arr[][] = new int[N][N];
+			int num = 1;
+			for(int a=0; a<N; a++) {
+				arr[0][a] = num;
+				num++;
+				if(a==(N-1)) {
+					for(int b=0; b<N; b++) {
+						arr=[]
+					}
+				}
+			}
+		}
+		
+	}
+}
