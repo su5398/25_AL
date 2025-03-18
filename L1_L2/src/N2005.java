3252f21ae6c94fcc54d29aec5a79bd73f23282d4
Kweon <su539@naver.com>
2025-03-19 00:46:29 +0900

    N2005

--- L1_L2/src/N2005.java
+++ L1_L2/src/N2005.java
@@ -1,9 +1,37 @@
+import java.util.Scanner;
 
 public class N2005 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		
+		for(int test_case=1; test_case<=T; test_case++) {
+			int N = sc.nextInt();
+			
+			int[][] arr = new int[N][N];
+			
+			for(int i=0; i<N; i++) {
+				for(int j=0; j<=i; j++) {
+					if(i==j) {
+						arr[i][j] = 1;
+					}else if(j==0) {
+						arr[i][j] = 1;
+					}else {
+						arr[i][j] = arr[i-1][j-1]+arr[i-1][j];
+					}
+				}
+			}
+			
+			System.out.println("#"+test_case);
+			for(int i=0; i<N; i++) {
+				for(int j=0; j<=i; j++) {
+					System.out.print(arr[i][j]+" ");
+				}
+				System.out.println();
+			}
+		}
 	}
 
 }
